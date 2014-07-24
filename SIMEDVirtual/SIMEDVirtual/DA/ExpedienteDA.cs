c8b7185128069c410fc49c80a41c6eea7e9e7b6c
Karen <karenbol17@gmail.com>
2014-07-24 14:18:29 -0600

    prueba de eliminacion de cuenta

--- SIMEDVirtual/SIMEDVirtual/DA/ExpedienteDA.cs
+++ SIMEDVirtual/SIMEDVirtual/DA/ExpedienteDA.cs
@@ -49,6 +49,7 @@ namespace SIMEDVirtual.DA
         }
 
         //comentarios ade prueba bitbucked
+        //////al precer no funciona
 
         //metodo get all de reconsultas
         public static List<ExpedienteEntity> selectExpediente(string cedula_cliente)
